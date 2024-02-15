4e444688e9897eeac0df9d941a0c523a8561e87c
1MishGun1 <mikhail.kudrov@gmail.com>
2024-02-15 11:21:44 +0300

    update CardsList.jsx

--- app/components/CardsList/CardsList.jsx
+++ app/components/CardsList/CardsList.jsx
@@ -1,5 +1,14 @@
-const CardsList = (props) => {
-  return <div>CardsList</div>;
+import Styles from './CardsList.module.css';
+
+export const CardsList = (props) => {
+  return (
+    <section className={Styles['list-section']}>
+      <h2 className={Styles['list-section__title']} id={props.id}>
+        {props.title}
+      </h2>
+      <ul className={Styles['cards-list']}>{props.children}</ul>
+    </section>
+  );
 };
 
 export default CardsList;
