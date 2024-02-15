7dffb2ba1925849108e3f822c53f08d17fbe3698
1MishGun1 <mikhail.kudrov@gmail.com>
2024-02-15 11:21:32 +0300

    update page.js

--- app/page.js
+++ app/page.js
@@ -2,16 +2,21 @@ import Image from 'next/image';
 import styles from './page.module.css';
 
 import { Banner } from './components/Banner/Banner';
-import { PopularCardsList } from './components/CardsList/PopularCardsList';
-import { NewCardsList } from './components/CardsList/NewCardsList';
+import { CardsList } from './components/CardsList/CardsList';
+import { PopularCardsFragment } from './components/CardsList/PopularCardsFragment';
+import { NewCardsFragment } from './components/CardsList/NewCardsFragment';
 import { Promo } from './components/Promo/Promo';
 
 export default function Home() {
   return (
     <main className="main">
       <Banner />
-      <PopularCardsList />
-      <NewCardsList />
+      <CardsList id="popular" title="Популярные">
+        <PopularCardsFragment />
+      </CardsList>
+      <CardsList id="new" title="Новинки">
+        <NewCardsFragment />
+      </CardsList>
       <Promo />
     </main>
   );
