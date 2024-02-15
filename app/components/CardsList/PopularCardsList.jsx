6e0c309a036f8e429e4b3bbb86cdccc591fbca54
1MishGun1 <mikhail.kudrov@gmail.com>
2024-02-15 10:48:07 +0300

    update PopularCardsList.jsx

--- app/components/CardsList/PopularCardsList.jsx
+++ app/components/CardsList/PopularCardsList.jsx
@@ -1,7 +1,17 @@
 import { PopularCardsFragment } from './PopularCardsFragment';
+import Styles from './CardsList.module.css';
 
 export const PopularCardsList = () => {
-  return <PopularCardsFragment />;
+  return (
+    <section className={Styles['list-section']}>
+      <h2 className={Styles['list-section__title']} id="popular">
+        Популярное
+      </h2>
+      <ul className={Styles['cards-list']}>
+        <PopularCardsFragment />;
+      </ul>
+    </section>
+  );
 };
 
 export default PopularCardsList;
