c5765bed956634f478d7b8e118509057ef05b7e9
1MishGun1 <mikhail.kudrov@gmail.com>
2024-02-29 22:45:17 +0300

    update data-utils.js

--- app/data/data-utils.js
+++ app/data/data-utils.js
@@ -9,7 +9,5 @@ export const getGamesByCategory = (category) => {
 };
 
 export const getGameById = (id) => {
-  return data.find((game) => {
-    return game.id === Number(id);
-  });
+  return data.find((game) => game.id === Number(id));
 };
