6341c0d77d8df84132652bddd7b5392384bd0081
1MishGun1 <mikhail.kudrov@gmail.com>
2024-02-29 22:45:35 +0300

    update page.js [id]

--- app/games/[id]/page.js
+++ app/games/[id]/page.js
@@ -1,42 +1,37 @@
-import { getGameById } from '../../data/data-utils';
-import Styles from './Game.module.css';
+"use client";
+import { getGameById } from "@/app/data/data-utils";
+import Styles from "./Game.module.css";
 
-export default function GamePage(props) {
-  const game = getGameById();
-
-  return game ? (
-    <>
-      <section className={Styles['game']}>
-        <iframe className={Styles['game__iframe']} src={game.link}></iframe>
+const GamePage = (props) => {
+  const game = getGameById(props.params.id);
+  return (
+    <main className="main">
+      <section className={Styles["game"]}>
+        <iframe className={Styles["game__iframe"]} src={game.link}></iframe>
       </section>
-      <section className={Styles['about']}>
-        <h2 className={Styles['about__title']}>{game.title}</h2>
-        <div className={Styles['about__content']}>
-          <p className={Styles['about__description']}>{game.description}</p>
-          <div className={Styles['about__author']}>
+      <section className={Styles["about"]}>
+        <h2 className={Styles["about__title"]}>{game.title}</h2>
+        <div className={Styles["about__content"]}>
+          <p className={Styles["about__description"]}>{game.description}</p>
+          <div className={Styles["about__author"]}>
             <p>
-              Автор:{' '}
-              <span className={Styles['about__accent']}>{game.developer}</span>
+              Автор:
+              <span className={Styles["about__accent"]}>{game.developer}</span>
             </p>
           </div>
         </div>
-        <div className={Styles['about__vote']}>
-          <p className={Styles['about__vote-amount']}>
-            За игру уже проголосовали:{' '}
-            <span className={Styles['about__accent']}>{game.users.length}</span>
+        <div className={Styles["about__vote"]}>
+          <p className={Styles["about__vote-amount"]}>
+            За игру уже проголосовали:
+            <span className={Styles["about__accent"]}>{game.users.length}</span>
           </p>
-          <button
-            onClick={() => {}}
-            className={`button ${Styles['about__vote-button']}`}
-          >
+          <button className={`button ${Styles["about__vote-button"]}`}>
             Голосовать
           </button>
         </div>
       </section>
-    </>
-  ) : (
-    <section className={Styles['game']}>
-      <p>Такой игры не существует 😢</p>
-    </section>
+    </main>
   );
-}
+};
+
+export default GamePage;
