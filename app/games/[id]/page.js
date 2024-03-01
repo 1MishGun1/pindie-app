51915281a41a06da0df907191675ff4db8c448a1
1MishGun1 <mikhail.kudrov@gmail.com>
2024-03-01 23:24:46 +0300

    update page.js [id]

--- app/games/[id]/page.js
+++ app/games/[id]/page.js
@@ -1,8 +1,9 @@
-'use client';
-import { getGameById } from '@/app/data/data-utils';
-import Styles from './Game.module.css';
-import { GameNotFound } from '@/app/components/GameNotFound/GameNotFound';
-import { useEffect, useState } from 'react';
+"use client";
+import { getGameById } from "@/app/data/data-utils";
+import Styles from "./Game.module.css";
+import { GameNotFound } from "@/app/components/GameNotFound/GameNotFound";
+import { useEffect, useState } from "react";
+// import { useRouter } from "next/router";
 
 const GamePage = (props) => {
   const [game, setGame] = useState();
@@ -12,36 +13,38 @@ const GamePage = (props) => {
     setGame(fetchedGame);
   }, [props.params.id]);
 
+  // const router = useRouter();
+
   return (
     <main className="main">
       {game ? (
         <>
-          <section className={Styles['game']}>
-            <iframe className={Styles['game__iframe']} src={game.link}></iframe>
+          <section className={Styles["game"]}>
+            <iframe className={Styles["game__iframe"]} src={game.link}></iframe>
           </section>
-          <section className={Styles['about']}>
-            <h2 className={Styles['about__title']}>{game.title}</h2>
-            <div className={Styles['about__content']}>
-              <p className={Styles['about__description']}>{game.description}</p>
-              <div className={Styles['about__author']}>
+          <section className={Styles["about"]}>
+            <h2 className={Styles["about__title"]}>{game.title}</h2>
+            <div className={Styles["about__content"]}>
+              <p className={Styles["about__description"]}>{game.description}</p>
+              <div className={Styles["about__author"]}>
                 <p>
-                  Автор:{' '}
-                  <span className={Styles['about__accent']}>
+                  Автор:{" "}
+                  <span className={Styles["about__accent"]}>
                     {game.developer}
                   </span>
                 </p>
               </div>
             </div>
-            <div className={Styles['about__vote']}>
-              <p className={Styles['about__vote-amount']}>
-                За игру уже проголосовали:{' '}
-                <span className={Styles['about__accent']}>
+            <div className={Styles["about__vote"]}>
+              <p className={Styles["about__vote-amount"]}>
+                За игру уже проголосовали:{" "}
+                <span className={Styles["about__accent"]}>
                   {game.users.length}
                 </span>
               </p>
               <button
                 onClick={() => {}}
-                className={`button ${Styles['about__vote-button']}`}
+                className={`button ${Styles["about__vote-button"]}`}
               >
                 Голосовать
               </button>
@@ -49,7 +52,7 @@ const GamePage = (props) => {
           </section>
         </>
       ) : (
-        <section className={Styles['game']}>
+        <section className={Styles["game"]}>
           <GameNotFound />
         </section>
       )}
