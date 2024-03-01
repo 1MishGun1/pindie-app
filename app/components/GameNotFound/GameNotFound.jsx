1ee9ce8ac754b038c522b181c6123dbf3fa9e693
1MishGun1 <mikhail.kudrov@gmail.com>
2024-03-01 23:24:19 +0300

    update GameNotFound.jsx

--- app/components/GameNotFound/GameNotFound.jsx
+++ app/components/GameNotFound/GameNotFound.jsx
@@ -1,11 +1,14 @@
-import Styles from './GameNotFound.module.css';
-import { NotFoundImage } from './not-found';
+import Styles from "./GameNotFound.module.css";
+import { NotFoundImage } from "./not-found";
 
 export const GameNotFound = () => {
   return (
-    <div className={Styles['not-found']}>
+    <div className={Styles["not-found"]}>
       <NotFoundImage />
-      <h2 className={Styles['not-found__text']}>Такой игры не существует :(</h2>
+      <h2 className={Styles["not-found__text"]}>
+        Упс... <br />
+        Такой игры не существует :(
+      </h2>
     </div>
   );
 };
