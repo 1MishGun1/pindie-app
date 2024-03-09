33389815e250e2f0a3f470bff6e1ccc397df22f7
1MishGun1 <mikhail.kudrov@gmail.com>
2024-03-09 20:36:40 +0300

    update page.js

--- app/page.js
+++ app/page.js
@@ -1,15 +1,45 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
 
 import { getGamesByCategory } from "./data/data-utils.js";
-
 import { Banner } from "./components/Banner/Banner";
 import { CardsList } from "./components/CardsList/CardsList";
-import { PopularCardsFragment } from "./components/CardsList/PopularCardsFragment";
-import { NewCardsFragment } from "./components/CardsList/NewCardsFragment";
 import { Promo } from "./components/Promo/Promo";
+import { useEffect } from "react";
+
+// export const getData = async (url) => {
+//   try {
+//     const response = await fetch(url);
+//     if (response.status !== 200) {
+//       throw new Error("Ошибка получения данных");
+//     }
+//     const data = await response.json();
+//     return data;
+//   } catch (error) {
+//     return error;
+//   }
+// };
 
 export default function Home() {
+  // const dataFormUrl = await getData(
+  //   "https://api-code-2.practicum-team.ru/games"
+  // );
+  // console.log(dataFormUrl);
+  useEffect(() => {
+    const getData = async (url) => {
+      try {
+        const response = await fetch(url);
+        if (response.status !== 200) {
+          throw new Error("Ошибка получения данных");
+        }
+        const data = await response.json();
+        return data;
+      } catch (error) {
+        return error;
+      }
+    };
+    getData("https://api-code-2.practicum-team.ru/games");
+  }, []);
+
   const popularGames = getGamesByCategory("popular");
   const newGames = getGamesByCategory("new");
 
