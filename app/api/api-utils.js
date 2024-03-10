fb40f0600de13b65266ca15d432d18b64d16fd53
1MishGun1 <mikhail.kudrov@gmail.com>
2024-03-10 22:18:33 +0300

    api-utils.js

---
+++ app/api/api-utils.js
@@ -0,0 +1,116 @@
+const getData = async (url) => {
+  try {
+    const response = await fetch(url);
+    if (response.status !== 200) {
+      throw new Error("Ошибка получения данных");
+    }
+    const data = await response.json();
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.error("Ошибка: ", error.message);
+  }
+};
+
+const normalizeDataObject = (obj) => {
+  return {
+    ...obj,
+    category: obj.categories,
+    users: obj.users_permissions_users,
+  };
+};
+
+export const normalizeData = (data) => {
+  return data.map((item) => {
+    return normalizeDataObject(item);
+  });
+};
+
+export const isResponseOk = (response) => {
+  return !(response instanceof Error);
+};
+
+export const getNormalizedGameDataById = async (url, id) => {
+  const data = await getData(`${url}/${id}`);
+  return isResponseOk(data) ? normalizeDataObject(data) : data;
+  s;
+};
+
+export const getNormalizedGamesDataByCategory = async (url, category) => {
+  const data = await getData(`${url}?categories.name=${category}`);
+  return isResponseOk(data) ? normalizeData(data) : data;
+};
+
+// Authorization user
+export const authorize = async (url, data) => {
+  try {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data),
+    });
+    if (response.status !== 200) {
+      throw new Error("Ошибка авторизации");
+    }
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    return error;
+  }
+};
+
+// Get information user
+export const getMe = async (url, jwt) => {
+  try {
+    const response = await fetch(url, {
+      method: "GET",
+      headers: { Authorization: `Bearer ${jwt}` },
+    });
+    if (response.status !== 200) {
+      throw new Error("Ошибка получения данных");
+    }
+    const result = response.json();
+    return result;
+  } catch (error) {
+    return error;
+  }
+};
+
+// JWT token
+export const setJWT = (jwt) => {
+  localStorage.setItem("jwt", jwt);
+};
+
+export const getJWT = () => {
+  return localStorage.getItem("jwt");
+};
+
+export const removeJWT = () => {
+  localStorage.removeItem("jwt");
+};
+
+// Check auth user
+export const checkIfUserVoted = (game, userId) => {
+  return game.users.find((user) => user.id === userId);
+};
+
+// Update votes
+export const vote = async () => {
+  try {
+    const response = await fetch(url, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${jwt}`,
+      },
+      body: JSON.stringify({ users_permissions_users: usersArray }),
+    });
+    if (response.status !== 200) {
+      throw new Error("Ошибка голосования");
+    }
+    const result = response.json();
+    return result;
+  } catch (error) {
+    return error;
+  }
+};
