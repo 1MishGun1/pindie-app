ca7013c41b10541b3c6f4c67eef96d5cce70524a
1MishGun1 <mikhail.kudrov@gmail.com>
2024-03-12 13:37:44 +0300

    update page.js (game page)

--- app/games/[id]/page.js
+++ app/games/[id]/page.js
@@ -1,10 +1,9 @@
-"use client";
+'use client';
 
-import Styles from "./Game.module.css";
-import { GameNotFound } from "@/app/components/GameNotFound/GameNotFound";
-import { useEffect, useState } from "react";
-import { useRouter } from "next/navigation";
-import { endpoints } from "@/app/api/config";
+import Styles from './Game.module.css';
+import { GameNotFound } from '@/app/components/GameNotFound/GameNotFound';
+import { useEffect, useState } from 'react';
+import { endpoints } from '@/app/api/config';
 import {
   getJWT,
   removeJWT,
@@ -13,8 +12,8 @@ import {
   getNormalizedGameDataById,
   checkIfUserVoted,
   vote,
-} from "@/app/api/api-utils";
-import Preloader from "@/app/components/Preloader/Preloader";
+} from '@/app/api/api-utils';
+import Preloader from '@/app/components/Preloader/Preloader';
 
 const GamePage = (props) => {
   const [game, setGame] = useState(null);
@@ -22,7 +21,6 @@ const GamePage = (props) => {
   const [isAuthorized, setIsAuthorized] = useState(false);
   const [currentUser, setCurrentUser] = useState(null);
   const [isVoted, setIsVoted] = useState(false);
-  const router = useRouter();
 
   useEffect(() => {
     async function fetchData() {
@@ -82,43 +80,39 @@ const GamePage = (props) => {
     }
   };
 
-  const handleClick = () => {
-    router.push("/login");
-  };
-
   return (
     <main className="main">
       {game ? (
         <>
-          <section className={Styles["game"]}>
-            <iframe className={Styles["game__iframe"]} src={game.link}></iframe>
+          <section className={Styles['game']}>
+            <iframe className={Styles['game__iframe']} src={game.link}></iframe>
           </section>
-          <section className={Styles["about"]}>
-            <h2 className={Styles["about__title"]}>{game.title}</h2>
-            <div className={Styles["about__content"]}>
-              <p className={Styles["about__description"]}>{game.description}</p>
-              <div className={Styles["about__author"]}>
+          <section className={Styles['about']}>
+            <h2 className={Styles['about__title']}>{game.title}</h2>
+            <div className={Styles['about__content']}>
+              <p className={Styles['about__description']}>{game.description}</p>
+              <div className={Styles['about__author']}>
                 <p>
-                  Автор:{" "}
-                  <span className={Styles["about__accent"]}>
+                  Автор:{' '}
+                  <span className={Styles['about__accent']}>
                     {game.developer}
                   </span>
                 </p>
               </div>
             </div>
-            <div className={Styles["about__vote"]}>
-              <p className={Styles["about__vote-amount"]}>
-                За игру уже проголосовали:{" "}
-                <span className={Styles["about__accent"]}>
+            <div className={Styles['about__vote']}>
+              <p className={Styles['about__vote-amount']}>
+                За игру уже проголосовали:{' '}
+                <span className={Styles['about__accent']}>
                   {game.users.length}
                 </span>
               </p>
               <button
                 disabled={!isAuthorized || isVoted}
-                className={`button ${Styles["about__vote-button"]}`}
+                className={`button ${Styles['about__vote-button']}`}
                 onClick={handleVote}
               >
-                {isVoted ? "Голос учтён" : "Голосовать"}
+                {isVoted ? 'Голос учтён' : 'Голосовать'}
               </button>
             </div>
           </section>
@@ -126,7 +120,7 @@ const GamePage = (props) => {
       ) : preloaderVisible ? (
         <Preloader />
       ) : (
-        <section className={Styles["game"]}>
+        <section className={Styles['game']}>
           <GameNotFound />
         </section>
       )}
