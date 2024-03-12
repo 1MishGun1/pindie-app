48c947f527b6a1fd6e85a2bdcba11b958f64638b
1MishGun1 <mikhail.kudrov@gmail.com>
2024-03-12 13:37:21 +0300

    update page.js (main page)

--- app/page.js
+++ app/page.js
@@ -1,48 +1,13 @@
-"use client";
+'use client';
 
-import { getGamesByCategory } from "./data/data-utils.js";
-import { Banner } from "./components/Banner/Banner";
-import { CardsList } from "./components/CardsList/CardsList";
-import { Promo } from "./components/Promo/Promo";
-import { useEffect } from "react";
-
-// export const getData = async (url) => {
-//   try {
-//     const response = await fetch(url);
-//     if (response.status !== 200) {
-//       throw new Error("Ошибка получения данных");
-//     }
-//     const data = await response.json();
-//     return data;
-//   } catch (error) {
-//     return error;
-//   }
-// };
+import { getNormalizedGamesDataByCategory } from './api/api-utils.js';
+import { Banner } from './components/Banner/Banner';
+import { CardsList } from './components/CardsList/CardsList';
+import { Promo } from './components/Promo/Promo';
 
 export default function Home() {
-  // const dataFormUrl = await getData(
-  //   "https://api-code-2.practicum-team.ru/games"
-  // );
-  // console.log(dataFormUrl);
-  useEffect(() => {
-    const getData = async (url) => {
-      try {
-        const response = await fetch(url);
-        if (response.status !== 200) {
-          throw new Error("Ошибка получения данных");
-        }
-        const data = await response.json();
-        console.log(data);
-        return data;
-      } catch (error) {
-        return error;
-      }
-    };
-    getData("https://api-code-2.practicum-team.ru/games");
-  }, []);
-
-  const popularGames = getGamesByCategory("popular");
-  const newGames = getGamesByCategory("new");
+  const popularGames = getNormalizedGamesDataByCategory('popular');
+  const newGames = getNormalizedGamesDataByCategory('new');
 
   return (
     <main className="main">
