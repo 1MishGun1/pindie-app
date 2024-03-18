a85180719654ed89da55fa7f5900456b9cdaad67
1MishGun1 <mikhail.kudrov@gmail.com>
2024-03-18 21:22:31 +0300

    update App.jsx

--- app/App.jsx
+++ app/App.jsx
@@ -9,51 +9,55 @@ import { endpoints } from "./api/config";
 
 import { AuthContext } from "./context/app-context";
 
+import { useStore } from "./store/app-store";
+
 export const App = (props) => {
   const [isAuth, setIsAuth] = useState(false);
   const [user, setUser] = useState(null);
   const [token, setToken] = useState("");
+  const store = useStore();
 
-  const login = (user, token) => {
-    setIsAuth(true);
-    setUser(user);
-    setToken(token);
-    setJWT(token);
-  };
+  //   const login = (user, token) => {
+  //     setIsAuth(true);
+  //     setUser(user);
+  //     setToken(token);
+  //     setJWT(token);
+  //   };
 
-  const logout = () => {
-    setIsAuth(false);
-    setUser(null);
-    setToken("");
-    removeJWT();
-  };
+  //   const logout = () => {
+  //     setIsAuth(false);
+  //     setUser(null);
+  //     setToken("");
+  //     removeJWT();
+  //   };
 
-  const checkAuth = async () => {
-    const token = getJWT();
-    if (token) {
-      const me = await getMe(endpoints.me, token);
-      if (me) {
-        login(me, token);
-      } else {
-        logout();
-      }
-    }
-  };
+  //   const checkAuth = async () => {
+  //     const token = getJWT();
+  //     if (token) {
+  //       const me = await getMe(endpoints.me, token);
+  //       if (me) {
+  //         login(me, token);
+  //       } else {
+  //         logout();
+  //       }
+  //     }
+  //   };
 
   useEffect(() => {
-    async function check() {
-      await checkAuth();
-    }
-    check();
+    // async function check() {
+    //   await checkAuth();
+    // }
+    // check();
+    store.checkAuth();
   }, []);
 
   return (
     <>
-      <AuthContext.Provider value={{ isAuth, user, token, login, logout }}>
-        <Header />
-        {props.children}
-        <Footer />
-      </AuthContext.Provider>
+      {/* <AuthContext.Provider value={{ isAuth, user, token, login, logout }}> */}
+      <Header />
+      {props.children}
+      <Footer />
+      {/* </AuthContext.Provider> */}
     </>
   );
 };
