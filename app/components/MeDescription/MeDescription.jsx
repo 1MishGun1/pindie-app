1f7e18dc97584df2c543096ec32c47a43454e78b
1MishGun1 <mikhail.kudrov@gmail.com>
2024-03-19 23:24:20 +0300

    MeDescription.jsx

---
+++ app/components/MeDescription/MeDescription.jsx
@@ -0,0 +1,30 @@
+"use client";
+
+import Styles from "./MeDescription.module.css";
+import { useStore } from "@/app/store/app-store";
+
+export const MeDescription = () => {
+  const authContext = useStore();
+  const handleLogout = () => {
+    authContext.logout();
+  };
+  return (
+    <section className={Styles["about__page_user"]}>
+      <h2 className={Styles["about__page_title"]}>Страница аккаунта</h2>
+      <div className={Styles["about__page_field"]}>
+        <h4 className={Styles["about__page_desc"]}>Никнейм: </h4>
+        <p className={Styles["about__page_answer"]}>Ник</p>
+      </div>
+      <div className={Styles["about__page_field"]}>
+        <h4 className={Styles["about__page_desc"]}>Email: </h4>
+        <p className={Styles["about__page_answer"]}>Email</p>
+      </div>
+      <button
+        onClick={handleLogout}
+        className={`button ${Styles["about__exit-button"]}`}
+      >
+        Выход
+      </button>
+    </section>
+  );
+};
