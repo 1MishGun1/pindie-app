7c1802e150959e9ab760f7b8502eeb2d67361b6b
1MishGun1 <mikhail.kudrov@gmail.com>
2024-03-20 12:09:54 +0300

    update MeDescription.jsx

--- app/components/MeDescription/MeDescription.jsx
+++ app/components/MeDescription/MeDescription.jsx
@@ -1,27 +1,32 @@
-"use client";
+'use client';
 
-import Styles from "./MeDescription.module.css";
-import { useStore } from "@/app/store/app-store";
+import Styles from './MeDescription.module.css';
+import { useStore } from '@/app/store/app-store';
+import { useRouter } from 'next/navigation';
 
 export const MeDescription = () => {
   const authContext = useStore();
+  const router = useRouter();
+
   const handleLogout = () => {
+    router.push('/');
     authContext.logout();
   };
+
   return (
-    <section className={Styles["about__page_user"]}>
-      <h2 className={Styles["about__page_title"]}>Страница аккаунта</h2>
-      <div className={Styles["about__page_field"]}>
-        <h4 className={Styles["about__page_desc"]}>Никнейм: </h4>
-        <p className={Styles["about__page_answer"]}>Ник</p>
+    <section className={Styles['about__page_user']}>
+      <h2 className={Styles['about__page_title']}>Страница аккаунта</h2>
+      <div className={Styles['about__page_field']}>
+        <h4 className={Styles['about__page_desc']}>Никнейм: </h4>
+        <p className={Styles['about__page_answer']}>Ник</p>
       </div>
-      <div className={Styles["about__page_field"]}>
-        <h4 className={Styles["about__page_desc"]}>Email: </h4>
-        <p className={Styles["about__page_answer"]}>Email</p>
+      <div className={Styles['about__page_field']}>
+        <h4 className={Styles['about__page_desc']}>Email: </h4>
+        <p className={Styles['about__page_answer']}>Email</p>
       </div>
       <button
         onClick={handleLogout}
-        className={`button ${Styles["about__exit-button"]}`}
+        className={`button ${Styles['about__exit-button']}`}
       >
         Выход
       </button>
