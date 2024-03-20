588963f39f4e732c9869e83d8d8a74c35e141a54
1MishGun1 <mikhail.kudrov@gmail.com>
2024-03-20 21:10:51 +0300

    update MeDescription.jsx

--- app/components/MeDescription/MeDescription.jsx
+++ app/components/MeDescription/MeDescription.jsx
@@ -1,32 +1,49 @@
-'use client';
+"use client";
 
-import Styles from './MeDescription.module.css';
-import { useStore } from '@/app/store/app-store';
-import { useRouter } from 'next/navigation';
+import Styles from "./MeDescription.module.css";
+import { useStore } from "@/app/store/app-store";
+import { useRouter } from "next/navigation";
+// import { authorize } from "@/app/api/api-utils";
+// import { endpoints } from "@/app/api/config";
+// import { useEffect } from "react";
 
 export const MeDescription = () => {
   const authContext = useStore();
   const router = useRouter();
 
   const handleLogout = () => {
-    router.push('/');
+    router.push("/");
     authContext.logout();
   };
 
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     try {
+  //       const response = await authorize(endpoints.auth, authData);
+  //       const data = await response.json();
+  //       setResult(data);
+  //     } catch (error) {
+  //       console.error("Error fetching data:", error);
+  //     }
+  //   };
+
+  //   fetchData();
+  // }, []);
+
   return (
-    <section className={Styles['about__page_user']}>
-      <h2 className={Styles['about__page_title']}>Страница аккаунта</h2>
-      <div className={Styles['about__page_field']}>
-        <h4 className={Styles['about__page_desc']}>Никнейм: </h4>
-        <p className={Styles['about__page_answer']}>Ник</p>
+    <section className={Styles["about__page_user"]}>
+      <h2 className={Styles["about__page_title"]}>Страница аккаунта</h2>
+      <div className={Styles["about__page_field"]}>
+        <h4 className={Styles["about__page_desc"]}>Юзернэйм: </h4>
+        <p className={Styles["about__page_answer"]}>Юзернэйм</p>
       </div>
-      <div className={Styles['about__page_field']}>
-        <h4 className={Styles['about__page_desc']}>Email: </h4>
-        <p className={Styles['about__page_answer']}>Email</p>
+      <div className={Styles["about__page_field"]}>
+        <h4 className={Styles["about__page_desc"]}>Email: </h4>
+        <p className={Styles["about__page_answer"]}>Email</p>
       </div>
       <button
         onClick={handleLogout}
-        className={`button ${Styles['about__exit-button']}`}
+        className={`button ${Styles["about__exit-button"]}`}
       >
         Выход
       </button>
