82ff151bcba349be623b1432ff99a3a2f9286eee
1MishGun1 <mikhail.kudrov@gmail.com>
2024-03-20 14:13:21 +0300

    update RegisterForm.jsx

--- app/components/RegisterForm/RegisterForm.jsx
+++ app/components/RegisterForm/RegisterForm.jsx
@@ -1,43 +1,57 @@
-"use client";
+'use client';
 
-import Styles from "./RegisterForm.module.css";
-import { useState } from "react";
+import Styles from './RegisterForm.module.css';
+import { useState } from 'react';
 
 export const RegisterForm = () => {
+  // const [registerData, setRegisterData] = useState({
+  //   nickname: '',
+  //   email: '',
+  //   password: '',
+  // });
+
+  // const handleClick = () => {
+  //   e.PreventDefault();
+
+  // }
+
   return (
-    <form className={Styles["form"]}>
-      <h2 className={Styles["form__title"]}>Регистрация</h2>
-      <div className={Styles["form__fields"]}>
-        <label className={Styles["form__field"]}>
-          <span className={Styles["form__field-title"]}>NickName</span>
+    <form className={Styles['form']}>
+      <h2 className={Styles['form__title']}>Регистрация</h2>
+      <div className={Styles['form__fields']}>
+        <label className={Styles['form__field']}>
+          <span className={Styles['form__field-title']}>NickName</span>
           <input
-            className={Styles["form__field-input"]}
+            className={Styles['form__field-input']}
+            name="nickname"
             type="nickname"
             placeholder="helloWorld"
           />
         </label>
-        <label className={Styles["form__field"]}>
-          <span className={Styles["form__field-title"]}>Email</span>
+        <label className={Styles['form__field']}>
+          <span className={Styles['form__field-title']}>Email</span>
           <input
-            className={Styles["form__field-input"]}
+            className={Styles['form__field-input']}
+            name="email"
             type="email"
             placeholder="hello@world.com"
           />
         </label>
-        <label className={Styles["form__field"]}>
-          <span className={Styles["form__field-title"]}>Пароль</span>
+        <label className={Styles['form__field']}>
+          <span className={Styles['form__field-title']}>Пароль</span>
           <input
-            className={Styles["form__field-input"]}
+            className={Styles['form__field-input']}
+            name="password"
             type="password"
             placeholder="***********"
           />
         </label>
       </div>
-      <div className={Styles["form__actions"]}>
-        <button className={Styles["form__reset"]} type="reset">
+      <div className={Styles['form__actions']}>
+        <button className={Styles['form__reset']} type="reset">
           Очистить
         </button>
-        <button className={Styles["form__submit"]} type="submit">
+        <button className={Styles['form__submit']} type="submit">
           Зарегистрироваться
         </button>
       </div>
