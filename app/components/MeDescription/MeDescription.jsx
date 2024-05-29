ec74436ed732ace133e20a32d3b55d19c7f7a625
1MishGun1 <mikhail.kudrov@gmail.com>
2024-05-29 11:07:21 +0300

    delete two useEffect

--- app/components/MeDescription/MeDescription.jsx
+++ app/components/MeDescription/MeDescription.jsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { useEffect, useState } from "react";
 import Styles from "./MeDescription.module.css";
 import { useRouter } from "next/navigation";
 import { useStore } from "@/app/store/app-store";
@@ -10,35 +9,11 @@ export const MeDescription = () => {
   const authContext = useStore();
   const router = useRouter();
   const store = useStore();
-  const [username, setUserName] = useState("");
-  const [email, setEmail] = useState("");
 
   const handleLogout = () => {
     router.push("/");
     authContext.logout();
   };
-
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      const initName = localStorage.getItem("username");
-      const initEmail = localStorage.getItem("email");
-
-      if (initName && initName !== username) {
-        setUserName(initName);
-      }
-
-      if (initEmail && initEmail !== email) {
-        setEmail(initEmail);
-      }
-    }
-  }, [store.user]);
-
-  useEffect(() => {
-    if (store.user && typeof window !== "undefined") {
-      localStorage.setItem("username", store.user.username);
-      localStorage.setItem("email", store.user.email);
-    }
-  }, [store.user]);
 
   if (!store.user) {
     return <Preloader />;
@@ -49,11 +24,11 @@ export const MeDescription = () => {
       <h2 className={Styles["about__page_title"]}>Страница аккаунта</h2>
       <div className={Styles["about__page_field"]}>
         <h4 className={Styles["about__page_desc"]}>Юзернэйм: </h4>
-        <p className={Styles["about__page_answer"]}>{username}</p>
+        <p className={Styles["about__page_answer"]}>{store.user?.username}</p>
       </div>
       <div className={Styles["about__page_field"]}>
         <h4 className={Styles["about__page_desc"]}>Email: </h4>
-        <p className={Styles["about__page_answer"]}>{email}</p>
+        <p className={Styles["about__page_answer"]}>{store.user?.email}</p>
       </div>
       <button
         onClick={handleLogout}
